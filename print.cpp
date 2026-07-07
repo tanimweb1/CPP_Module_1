e6a253d498a824978f18342375d08a123343862c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-07-07 18:02:36 +0600

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+
+int x = 100;
+double y = 34.5;
+char a = 'A';
+
+std::cout<<x<<" "<<y<<std::endl<<a<<std::endl;
+
+// my favorate number is 100
+
+std::cout<< "my favorite number is " << x<<std::endl;
+
+    return 0;
+}
