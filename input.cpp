f2ed8f788bb80946f3a145d1681db24b3439462e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-07-07 18:34:21 +0600

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+char a;
+//cin>>x>>a;
+//cout<<x <<" " << a <<endl;
+//  char b = 'A';
+// int asci  = b;
+// cout<<asci<<endl;
+
+char c = 'b';
+cout<< (int)c << endl;
+
+
+   return 0; 
+}
