2faaa199e07925513fec0f1860aa78dcbf65f450
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-07-08 15:51:03 +0600

    Add files via upload

---
+++ switch2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int x;
+    cin >> x;
+
+    switch (x % 2)
+    {
+
+    case 0:
+        cout << "Even\n";
+        break;
+    case 1:
+        cout << "Odd\n";
+        break;
+    }
+
+    return 0;
+}
